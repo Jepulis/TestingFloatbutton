d41d02fc6796752f5906bff22dec189c65171d5e
unknown <n4mani00@TYLD-EDLAB-G07.kk.lan>
2017-03-30 11:27:46 +0300

    Initial commit

---
+++ app/src/main/java/com/kidup/testingfloatbutton/MainActivity.java
@@ -0,0 +1,44 @@
+package com.kidup.testingfloatbutton;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.ShapeDrawable;
+import android.graphics.drawable.shapes.OvalShape;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+
+import com.getbase.floatingactionbutton.FloatingActionButton;
+import com.getbase.floatingactionbutton.FloatingActionsMenu;
+
+
+public class MainActivity extends AppCompatActivity  {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ShapeDrawable drawable = new ShapeDrawable(new OvalShape());
+        drawable.getPaint().setColor(getResources().getColor(R.color.white));
+
+
+        final FloatingActionButton actionA = (FloatingActionButton) findViewById(R.id.action_a);
+        actionA.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                actionA.setTitle("Action A clicked");
+            }
+        });
+
+
+    }
+
+    public void setTask(View view) {
+        // TODO
+        Intent intent = new Intent(this, SetTaskActivity.class);
+        startActivity(intent);
+    }
+
+}
---
+++ app/src/main/java/com/kidup/testingfloatbutton/SetTaskActivity.java
@@ -0,0 +1,13 @@
+package com.kidup.testingfloatbutton;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SetTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_set_task);
+    }
+}
